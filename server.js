ebece3b8d554d26b7b4f1ccb6a8fd19ec3e05e74
Navapallav Borthakur <navapallav.borthakur.cd.eee20@itbhu.ac.in>
2022-06-04 21:38:42 +0530

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connectDB=require('./server/database/connection');
 
 const app=express();
 dotenv.config({path:'config.env'});
-const PORT=process.env.PORT
+const PORT=process.env.PORT||8080
 
 app.use(morgan('tiny'));
 
