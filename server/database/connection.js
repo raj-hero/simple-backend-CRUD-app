decabb1520c34d8f193d1a7df2fb32915c2e4bf1
Navapallav Borthakur <navapallav.borthakur.cd.eee20@itbhu.ac.in>
2022-06-19 21:17:42 +0530

    Chaneg in mongo irl and dotenc

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+// const dotenv=require('dotenv');
+// const path=require('path');
+// dotenv.config({path:'config.env'});
 
 const connectDB = async () => {
     try{
         
-        const con = await mongoose.connect(process.env.MONGO_URI, {
+        const con = await mongoose.connect("mongodb://Raj:raj@cluster0-shard-00-00.br6qx.mongodb.net:27017,cluster0-shard-00-01.br6qx.mongodb.net:27017,cluster0-shard-00-02.br6qx.mongodb.net:27017/?ssl=true&replicaSet=atlas-geoosp-shard-0&authSource=admin&retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             // useFindAndModify: false,
